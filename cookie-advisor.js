c631fc7917e3a08d570d9add36d7af8f028cbdbf
Julien Halgand <julien.halgand1@gmail.com>
2017-05-25 23:37:06 +0200

    Add vendors files and cdn for jquery and $.cookie

--- cookie-advisor.js
+++ cookie-advisor.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-	if ($.cookie('cookieWarning') === undefined) {
-		$('body').append('<div class="row cookieWarningRow"><div class="panel cookieWarning" id="cookie">En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de Cookies pour réaliser des statistiques de visites anonymes. <a class="cookiewarning" target="_blank" href="http://www.google.com/intl/fr/policies/technologies/cookies/" rel="noindex">En savoir +</a> <button class="tiny radius cookieButton">Ok</button></div></div>');
-		$('div.cookieWarning').css({
-			'width'          : '100%',
-			'position' 			 : 'fixed',
-			'margin-left'        : 'auto',
-			'margin-right'       : 'auto',
-			'text-align'         : 'center',
-			'bottom'             : '0',
-			'padding'            : '20px',
-			'background'		 : '#3fb6e3',
-			'color'            	 : 'black',
-			'border-radius'      : '40px',
-			'opacity'            : '0.9',
-			'z-index'            : '5'
-	    });
-	    $('a.cookiewarning').css({
-	    	'color':'white'
-	    });
-	}
+$(document).ready(function() {
+    if ($.cookie('cookieWarning') === undefined) {
+        $('body').append('<div class="row cookieWarningRow"><div class="panel cookieWarning" id="cookie">En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de Cookies pour réaliser des statistiques de visites anonymes. <a class="cookiewarning" target="_blank" href="http://www.google.com/intl/fr/policies/technologies/cookies/" rel="noindex">En savoir +</a> <button class="tiny radius cookieButton">Ok</button></div></div>');
+        $('div.cookieWarning').css({
+            'width': '100%',
+            'position': 'fixed',
+            'margin-left': 'auto',
+            'margin-right': 'auto',
+            'text-align': 'center',
+            'bottom': '0',
+            'padding': '20px',
+            'background': '#3fb6e3',
+            'color': 'black',
+            //'border-radius'      	: '40px',
+            'opacity': '0.9',
+            'z-index': '5'
+        });
+        $('a.cookiewarning').css({
+            'color': 'white'
+        });
+    }
 
-    $('button.cookieButton').click(function(e){
-    	e.preventDefault();
-   		$.cookie('cookieWarning', 'viewed', {expires: 30*12});
-   		$('div.cookieWarningRow').hide();
-   	});
+    $('button.cookieButton').click(function(e) {
+        e.preventDefault();
+        $.cookie('cookieWarning', 'viewed', { expires: 30 * 12 });
+        $('div.cookieWarningRow').hide();
+    });
 });
